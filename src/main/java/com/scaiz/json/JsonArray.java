5ef681417bbc64014af3fe800d6dae8929b45c7c
zsc347 <zsc347@163.com>
2018-03-21 09:18:51 +0800

    json object implementation

--- src/main/java/com/scaiz/json/JsonArray.java
+++ src/main/java/com/scaiz/json/JsonArray.java
@@ -1,8 +1,14 @@
 package com.scaiz.json;
 
+import java.util.List;
+
 public class JsonArray {
 
   private Object list;
+
+  public JsonArray(List val) {
+
+  }
 
   public Object getList() {
     return list;
--- src/main/java/com/scaiz/json/JsonObject.java
+++ src/main/java/com/scaiz/json/JsonObject.java
@@ -1,10 +1,111 @@
 package com.scaiz.json;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 public class JsonObject {
 
-  private Object map;
+  private Map<String, Object> map;
 
-  public Object getMap() {
+  public JsonObject() {
+    map = new LinkedHashMap<>();
+  }
+
+  public JsonObject(Map<String, Object> map) {
+    this.map = map;
+  }
+
+  public Map<String, Object> getMap() {
     return map;
   }
+
+  @SuppressWarnings("unchecked")
+  public static JsonObject mapFrom(Object obj) {
+    return new JsonObject((Map<String, Object>)
+        Json.mapper.convertValue(obj, Map.class));
+  }
+
+  public <T> T convertTo(Class<T> type) {
+    return Json.mapper.convertValue(map, type);
+  }
+
+  public String getString(String key) {
+    Objects.requireNonNull(key);
+    CharSequence cs = (CharSequence) map.get(key);
+    return cs.toString();
+  }
+
+  public Integer getInteger(String key) {
+    Objects.requireNonNull(key);
+    Number number = (Number) map.get(key);
+    if (number == null) {
+      return null;
+    } else if (number instanceof Integer) {
+      return (Integer) number;
+    } else {
+      return number.intValue();
+    }
+  }
+
+  public Long getLong(String key) {
+    Objects.requireNonNull(key);
+    Number number = (Number) map.get(key);
+    if (number == null) {
+      return null;
+    } else if (number instanceof Long) {
+      return (Long) number;
+    } else {
+      return number.longValue();
+    }
+  }
+
+  public Double getDouble(String key) {
+    Objects.requireNonNull(key);
+    Number number = (Number) map.get(key);
+    if (number == null) {
+      return null;
+    } else if (number instanceof Double) {
+      return (Double) number;
+    } else {
+      return number.doubleValue();
+    }
+  }
+
+  public Float getFloat(String key) {
+    Objects.requireNonNull(key);
+    Number number = (Number) map.get(key);
+    if (number == null) {
+      return null;
+    } else if (number instanceof Float) {
+      return (Float) number;
+    } else {
+      return number.floatValue();
+    }
+  }
+
+  public Boolean getBoolean(String key) {
+    Objects.requireNonNull(key);
+    return (Boolean) map.get(key);
+  }
+
+  public JsonObject getJsonObject(String key) {
+    Objects.requireNonNull(key);
+    Object val = map.get(key);
+    if (val instanceof Map) {
+      val = new JsonObject((Map) val);
+    }
+    return (JsonObject) val;
+  }
+
+  public JsonArray getJsonArray(String key) {
+    Objects.requireNonNull(key);
+    Object val = map.get(key);
+    if (val instanceof List) {
+      val = new JsonArray((List) val);
+    }
+    return (JsonArray) val;
+  }
+
 }
