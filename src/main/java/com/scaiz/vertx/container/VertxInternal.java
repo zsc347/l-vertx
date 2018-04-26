8b8ee4e3f92716ab72c44d81270b0aabddf92432
zsc347 <zsc347@163.com>
2018-04-26 08:39:16 +0800

    add test for context 3

--- src/main/java/com/scaiz/vertx/container/VertxInternal.java
+++ src/main/java/com/scaiz/vertx/container/VertxInternal.java
@@ -6,7 +6,4 @@ import io.netty.channel.EventLoopGroup;
 public interface VertxInternal extends Vertx {
 
   EventLoopGroup getEventLoopGroup();
-
-
-
 }
--- src/test/java/com/scaiz/vertx/container/ContextTest.java
+++ src/test/java/com/scaiz/vertx/container/ContextTest.java
@@ -1,6 +1,8 @@
 package com.scaiz.vertx.container;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 import com.scaiz.vertx.container.impl.EventLoopContext;
@@ -60,6 +62,69 @@ public class ContextTest {
         latch.countDown();
       }
     });
+    latch.await(1, TimeUnit.MINUTES);
+    if (error[0] != null) {
+      throw new RuntimeException(error[0]);
+    }
+  }
+
+  @Test
+  public void testPutGetRemoveData() throws Exception {
+    Object obj = new Object();
+    Throwable[] error = new Throwable[1];
+    CountDownLatch latch = new CountDownLatch(1);
+    context.runOnContext(v -> {
+      Context ctx = ContextUtil.getCurrentThreadContext();
+      try {
+        assertSame(ctx, context);
+      } catch (Error e) {
+        error[0] = e;
+      }
+      ctx.put("foo", obj);
+      ctx.runOnContext(v2 -> {
+        try {
+          assertEquals(obj, ctx.get("foo"));
+          assertTrue(ctx.remove("foo"));
+        } catch (Error e) {
+          error[0] = e;
+        }
+        ctx.runOnContext(v3 -> {
+          try {
+            assertNull(ctx.get("foo"));
+          } catch (Exception e) {
+            error[0] = e;
+          }
+          latch.countDown();
+        });
+      });
+    });
+    latch.await(1, TimeUnit.MINUTES);
+    if (error[0] != null) {
+      throw new RuntimeException(error[0]);
+    }
+  }
+
+
+  @Test
+  public void testContextExceptionHandler() throws Exception {
+    RuntimeException failure = new RuntimeException();
+    Throwable[] error = new Throwable[1];
+    CountDownLatch latch = new CountDownLatch(1);
+
+    context.exceptionHandler(err -> {
+      try {
+        assertSame(context, ContextUtil.getCurrentThreadContext());
+        assertSame(failure, err);
+      } catch (Error e) {
+        error[0] = e;
+      }
+      latch.countDown();
+    });
+
+    context.runOnContext(v -> {
+      throw failure;
+    });
+
     latch.await(1, TimeUnit.MINUTES);
     if (error[0] != null) {
       throw new RuntimeException(error[0]);
